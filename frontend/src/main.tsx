1ee2d3b7f3828b7227ee2143799c7a81e430b421
Robert <robert.kaliszuk@gmail.com>
2026-05-07 15:19:22 +0200

    fix: remove unused Navigate import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 import { useAuthStore } from './stores/authStore';
